c804fbeb21c419e9307ebdf5f50f0e4d9c5f8b8f
gianmarcodpc <gianmarco.dello@mail.polimi.it>
2018-07-18 19:38:10 +0200

    functional additions to create graph

--- Advance Algorithms Project/main.cpp
+++ Advance Algorithms Project/main.cpp
@@ -1,31 +1,35 @@
 #include <iostream>
 #include "boost\graph\adjacency_list.hpp"
 
-
+boost::adjacency_list<>* createGraph(int numVertex);
+std::unique_ptr<boost::adjacency_list<>> createGraph2(int numVertex);
 
 int main() {
 	std::cout << "Advanced Algorithms Project" << std::endl;
+	
+	boost::adjacency_list<> *g;
+	g = createGraph(5);
+	
 	std::cin.get();
-	boost::adjacency_list<> g;
-	auto v1 = add_vertex(g);
-	auto v2 = add_vertex(g);
-	boost::adjacency_list<>::vertex_descriptor v3 = add_vertex(g);
-	
-	
-	
 	return 0;
 }
 
 
+
 boost::adjacency_list<>* createGraph(int numVertex) {
+
 
 	/*boost::adjacency_list<>* */ auto g = new boost::adjacency_list<>;
 	/* boost::adjacency_list<>::vertex_descriptor* */ auto v = new boost::adjacency_list<>::vertex_descriptor[numVertex];
 	for (int i = 0; i < numVertex; i++) {
 		v[i] = add_vertex(*g);
 	}
+	for (int i = 0; i < numVertex; i++) {
+		for (int j = i + 1; j < numVertex; j++) {
+			auto e = boost::add_edge(i, j, *g);
+		}
+	}
 	delete[] v;
-
 	return g;
 
 }
