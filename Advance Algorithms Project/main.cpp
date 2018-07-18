5ad27b039b49aeac39b350d38d8c2eb49098f84f
gianmarcodpc <gianmarco.dello@mail.polimi.it>
2018-07-18 18:27:51 +0200

    added boost graph support, and simple create graph method

--- Advance Algorithms Project/main.cpp
+++ Advance Algorithms Project/main.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
+#include "boost\graph\adjacency_list.hpp"
+
+
 
 int main() {
-	std::cout << "Advance Algorithms Project" << std::endl;
+	std::cout << "Advanced Algorithms Project" << std::endl;
 	std::cin.get();
+	boost::adjacency_list<> g;
+	auto v1 = add_vertex(g);
+	auto v2 = add_vertex(g);
+	boost::adjacency_list<>::vertex_descriptor v3 = add_vertex(g);
+	
+	
+	
 	return 0;
 }
+
+
+boost::adjacency_list<>* createGraph(int numVertex) {
+
+	boost::adjacency_list<>* g = new boost::adjacency_list<>;
+	boost::adjacency_list<>::vertex_descriptor *v = new boost::adjacency_list<>::vertex_descriptor[numVertex];
+	for (int i = 0; i < numVertex; i++) {
+		v[i] = add_vertex(*g);
+	}
+	delete[] v;
+
+}
+
+/*
+std::unique_ptr<boost::adjacency_list<>> createGraph(int numVertex) {
+
+	std::unique_ptr<boost::adjacency_list<>> g = std::make_unique<boost::adjacency_list<>>;
+	boost::adjacency_list<>::vertex_descriptor *v = new boost::adjacency_list<>::vertex_descriptor;
+	for (int i = 0; i < numVertex; i++) {
+		auto v1 = add_vertex(g);
+	}
+
+}
+*/
+
+
+
+
+
