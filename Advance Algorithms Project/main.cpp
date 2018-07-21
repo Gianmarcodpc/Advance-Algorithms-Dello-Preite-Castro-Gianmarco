dbf202f2068076dc12a97cc8ed332a94ff4e6ec7
gianmarcodpc <gianmarco.dello@mail.polimi.it>
2018-07-21 15:58:49 +0200

    Added random graph generation, and save graphviz function

--- Advance Algorithms Project/main.cpp
+++ Advance Algorithms Project/main.cpp
@@ -1,37 +1,64 @@
 #include <iostream>
-#include "boost\graph\adjacency_list.hpp"
+#include <boost\graph\adjacency_list.hpp>
+#include <time.h>
+#include <boost\random\mersenne_twister.hpp>
+#include <boost\random\uniform_int_distribution.hpp>
+#include <boost\graph\graphviz.hpp>
+#include <string>
 
-boost::adjacency_list<>* createGraph(int numVertex);
+typedef boost::adjacency_list	<boost::vecS, boost::vecS,boost::directedS,boost::no_property,boost::no_property> DirectedGraph;
+
+
+DirectedGraph* createRandomGraph(int numVertex);
 std::unique_ptr<boost::adjacency_list<>> createGraph2(int numVertex);
+void saveDirectedGraphToFile(DirectedGraph* g,int numVertex);
+
 
 int main() {
 	std::cout << "Advanced Algorithms Project" << std::endl;
-	
-	boost::adjacency_list<> *g;
-	g = createGraph(5);
-	
+	DirectedGraph* g;
+	g = createRandomGraph(5);
+	saveDirectedGraphToFile(g, 5);
+
+	delete g;
 	std::cin.get();
 	return 0;
 }
 
 
 
-boost::adjacency_list<>* createGraph(int numVertex) {
+DirectedGraph* createRandomGraph(int numVertex) {
 
+	boost::random::mt19937 gen(time(NULL));
+	boost::random::uniform_int_distribution<> dist(1, 10000);
+	int treshold = dist(gen);
+	std::cout << "Treshold " << treshold << std::endl;
 
-	/*boost::adjacency_list<>* */ auto g = new boost::adjacency_list<>;
+	/*DirectedGraph* */ auto g = new DirectedGraph;
 	/* boost::adjacency_list<>::vertex_descriptor* */ auto v = new boost::adjacency_list<>::vertex_descriptor[numVertex];
 	for (int i = 0; i < numVertex; i++) {
 		v[i] = add_vertex(*g);
 	}
-	for (int i = 0; i < numVertex; i++) {
-		for (int j = i + 1; j < numVertex; j++) {
-			auto e = boost::add_edge(i, j, *g);
+	for (int i = 0, randValue = 0; i < numVertex; i++) {
+		for (int j = 0; j < numVertex; j++) {
+			randValue = dist(gen);
+			if (randValue >= treshold) {
+				auto e = boost::add_edge(i, j, *g);
+				std::cout << "Added Edge from " << i << " to " << j << std::endl;
+			}
+			std::cout << "randValue " << j+i*numVertex << " " << randValue << std::endl;
 		}
 	}
 	delete[] v;
 	return g;
 
+}
+
+void saveDirectedGraphToFile(DirectedGraph* g, int numVertex) {
+	
+	std::ofstream writer("./DirectedGraphs/MyFirstDirectedGraph.txt");
+	boost::write_graphviz(writer, *g);
+	return;
 }
 
 
